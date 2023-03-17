dd79c243e08040d5b3558e04d7fc26192cc7d0c6
zhengx55 <williamzhxin@gmail.com>
2023-03-17 16:05:09 -0400

    btn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import Button, { ButtonSize, ButtonType } from "./components/Button";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Large
+      </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+        Small
+      </Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>
+        Danger
+      </Button>
+      <Button
+        btnType={ButtonType.Link}
+        size={ButtonSize.Large}
+        href="https://www.baidu.com"
+      >
+        Link
+      </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small} disabled>
+        Disabled
+      </Button>
+    </div>
+  );
 }
 
 export default App;
